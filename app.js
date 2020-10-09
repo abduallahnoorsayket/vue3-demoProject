c6e6a4c35879f02fafb1aaf3e92ab1380e211cff
abduallahnoorsayket <abdullahnoorsayket@gmail.com>
2020-10-09 17:34:44 +0600

    list & state transitions

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// For Flip images 
+
 const app = Vue.createApp({});
 
 
@@ -26,6 +28,7 @@ app.component("app-child", {
 
 app.mount("#app");
 
+//  For Toggle Box hello
 
 const Demo = {
 
@@ -72,15 +75,20 @@ const Demo = {
 
 Vue.createApp(Demo).mount('#demo')
 
+// For Toggle show message 
+
 const Demon = {
     data() {
         return {
-            show: true
+            showMessage: true
         }
     }
 }
 
 Vue.createApp(Demon).mount('#demon')
+
+
+// For toggle componenet
 
 const DemoCoponent = {
     data() {
@@ -90,24 +98,147 @@ const DemoCoponent = {
     },
     components: {
         "v-a": {
-            template: "<div>Component A</div>"
+            template: "<div>python is very good Programming Language</div>"
         },
         "v-b": {
-            template: "<div>Component B</div>"
+            template: "<div>PHP is a googd scripting language</div>"
         }
     }
 };
 
-Vue.createApp(Demo).mount("#demo");
+Vue.createApp(DemoCoponent).mount("#demoCompoent");
 
 
+// transiiton group shufle numbers
 
-// const Demo - bounce = {
-//     data() {
-//         return {
-//             show: true
-//         }
-//     }
-// }
+const DemoGroup = {
+    data() {
+        return {
+            items: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            nextNum: 10
+        }
+    },
+    methods: {
+        randomIndex() {
+            return Math.floor(Math.random() * this.items.length)
+        },
+        add() {
+            this.items.splice(this.randomIndex(), 0, this.nextNum++)
+        },
+        remove() {
+            this.items.splice(this.randomIndex(), 1)
+        },
+        shuffle() {
+            this.items = _.shuffle(this.items)
+        }
+    }
+};
 
-// Vue.createApp(Demo - bounce).mount('#demo-bounce')
+Vue.createApp(DemoGroup).mount("#list-complete-demo");
+
+// StagList names
+const DemoStagList = {
+    data() {
+        return {
+            query: '',
+            list: [
+                { msg: 'Bruce Lee' },
+                { msg: 'Badal' },
+                { msg: 'kamal' },
+                { msg: 'Kamrul' },
+                { msg: 'Kung Fury' },
+                { msg: 'jamal' },
+                { msg: 'jumman' },
+                { msg: 'joha' }
+
+
+            ]
+        }
+    },
+    computed: {
+        computedList() {
+            var vm = this
+            return this.list.filter((item) => {
+                return item.msg.toLowerCase().indexOf(vm.query.toLowerCase()) !== -1
+            })
+        }
+    },
+    methods: {
+        beforeEnter(el) {
+            el.style.opacity = 0
+            el.style.height = 0
+        },
+        enter(el, done) {
+            gsap.to(el, {
+                opacity: 1,
+                height: '1.6em',
+                delay: el.dataset.index * .15,
+                onComplete: done
+            })
+        },
+        leave(el, done) {
+            gsap.to(el, {
+                opacity: 0,
+                height: 0,
+                delay: el.dataset.index * .15,
+                onComplete: done
+            })
+        }
+    }
+}
+
+Vue.createApp(DemoStagList).mount('#demofilter')
+
+// for transition summation
+const sumapp = Vue.createApp({
+    data() {
+        return {
+            firstNumber: 20,
+            secondNumber: 40
+        }
+    },
+    computed: {
+        result() {
+            return this.firstNumber + this.secondNumber
+        }
+    }
+})
+
+sumapp.component('animated-integer', {
+    template: '<span>{{ fullValue }}</span>',
+    props: {
+        value: {
+            type: Number,
+            required: true
+        }
+    },
+    data() {
+        return {
+            tweeningValue: 0
+        }
+    },
+    computed: {
+        fullValue() {
+            return Math.floor(this.tweeningValue)
+        }
+    },
+    methods: {
+        tween(newValue, oldValue) {
+            gsap.to(this.$data, {
+                duration: 0.5,
+                tweeningValue: newValue,
+                ease: 'sine'
+            });
+        }
+    },
+    watch: {
+        value(newValue, oldValue) {
+            this.tween(newValue, oldValue)
+        }
+    },
+    mounted() {
+        this.tween(this.value, 0)
+    },
+})
+
+sumapp.mount('#sumapp')
